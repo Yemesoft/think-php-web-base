b442735038659c15b572fcb620d1fd5a55922b15
365168907@qq.com <365168907@qq.com>
2023-02-15 16:11:33 +0800

    -

--- web/src/views/admin/components/AppMenus.vue
+++ web/src/views/admin/components/AppMenus.vue
@@ -1,27 +1,330 @@
 <template>
-  <div>
+  <div style="display: flex;flex-direction: column">
     <el-radio-group v-model="isCollapse" style="margin-bottom: 20px">
       <el-radio-button :label="false">expand</el-radio-button>
       <el-radio-button :label="true">collapse</el-radio-button>
     </el-radio-group>
-    <el-menu
-        default-active="2"
-        class="el-menu-vertical-demo"
-        :collapse="isCollapse"
-        @open="handleOpen"
-        @close="handleClose"
-    >
-      <template v-for="(item,index) in menuItems" :key="index">
-        <router-link :to="item.path">
-          <el-menu-item v-if="!item.hidden" index="1">
-            <el-icon>
-              <Menu/>
-            </el-icon>
-            <template #title>{{ item.name }}</template>
-          </el-menu-item>
-        </router-link>
-      </template>
-    </el-menu>
+    <div style="overflow: auto;flex: 1">
+      <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          :collapse="isCollapse"
+          @open="handleOpen"
+          @close="handleClose"
+      >
+        <template v-for="(item,index) in menuItems" :key="index">
+          <router-link v-if="!item.hidden" :to="item.path">
+            <el-menu-item index="1" class="menu-item">
+              <el-icon>
+                <Menu/>
+              </el-icon>
+              <template #title><span>{{ item.name }}</span></template>
+            </el-menu-item>
+          </router-link>
+        </template>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+
+        <el-menu-item index="1" class="menu-item">
+          <el-icon>
+            <Menu/>
+          </el-icon>
+          <template #title><span>Test</span></template>
+        </el-menu-item>
+      </el-menu>
+    </div>
   </div>
 </template>
 
@@ -59,5 +362,17 @@ export default {
 .el-menu-vertical-demo:not(.el-menu--collapse) {
   width: 200px;
   min-height: 400px;
+}
+.menu-item{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  background-color: red;
+  text-align: start;
+
+  .menu-item-icon{
+    margin-right: 13px;
+  }
 }
 </style>
